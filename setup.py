9b5206efcaf8df365f89049826b1589c3c34962b
Maciej Dziardziel <m.dziardziel@cv-library.co.uk>
2014-11-11 10:38:05 +0000

    add tmux config

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ def setup_git():
 
 
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
-for fname in ('.vimrc', '.vim'):
+for fname in ('.vimrc', '.vim', '.tmux.conf'):
     full_name = os.path.join(os.path.expanduser('~/'), fname)
-    print 'Processing {0}'.format(full_name)
+    print('Processing {0}'.format(full_name))
     if os.path.exists(full_name):
-        print 'renaming {0} to {1}'.format(full_name, full_name+'_old')
+        print('    renaming {0} to {1}'.format(full_name, full_name+'_old'))
         os.rename(full_name, full_name+'_old')
-    print  os.path.join(BASEDIR, fname), 'fname', full_name
+    #print(os.path.join(BASEDIR, fname), 'fname', full_name)
     os.symlink(os.path.join(BASEDIR, fname), full_name)
 setup_git()
